7a2c967b23ec891052b20b236d45460e0e8d9eec
rodriguezmauricio <contato.mauricioalves@gmail.com>
2024-02-09 17:46:57 +0000

    footer css fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/footer/Footer";
 import BookATable from "./components/pages/BookATable";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
