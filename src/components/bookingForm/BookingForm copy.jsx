e2cde05c7441a95fbab98e5ce2787a56f41e6e74
rodriguezmauricio <contato.mauricioalves@gmail.com>
2024-02-11 00:17:30 +0000

    added new page to match the requirements for the course ¬¬ to lift up the state from the form

---
+++ src/components/bookingForm/BookingForm copy.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./bookingForm.css";
+import Button from "../button/Button";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+// set up the validation schema for the form
+const schema = yup.object({
+  date: yup
+    .date()
+    .typeError("Select your reservation date")
+    .required("Select your reservation date"),
+  time: yup
+    .string()
+    .required("Select reservation hour.")
+    .test("is-not-default", "Select time", (value) => value !== "Select time"),
+  numberOfGuests: yup
+    .number()
+    .typeError("Choose a number of guests from 1 to 10.")
+    .positive("Choose a number of guests from 1 to 10.")
+    .integer("Choose a number of guests from 1 to 10.")
+    .required("Choose a number of guests from 1 to 10.")
+    .min(1, "Choose a number of guests from 1 to 10.")
+    .max(10, "Choose a number of guests from 1 to 10."),
+  occasion: yup
+    .string()
+    .required("Let us know the occasion.")
+    .test("is-not-default", "Select an occasion", (value) => value !== "Select occasion"),
+});
+
+const BookingForm = () => {
+  const availableTimes = ["Select time", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  // const [formContent, setFormContent] = useState({
+  //   date: "",
+  //   time: "",
+  //   numberOfGuests: "0",
+  //   occasion: "Select Occasion",
+  // });
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  // console.log(watch("example")); // watch input value by passing the name of it
+
+  // const handleFormContent = (event) => {
+  //   setFormContent({ ...formContent, [event.target.name]: event.target.value });
+  //   console.log(event.target.value);
+  // };
+
+  const sendFormContent = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <h1 className="subtitle">Reserve a Table</h1>
+      <form className="booking-form-container" onSubmit={handleSubmit(sendFormContent)}>
+        <label htmlFor="res-date">Choose date</label>
+        <input className="booking-form-input" {...register("date")} type="date" />
+        <p className="error-message">{errors.date?.message}</p>
+
+        <label htmlFor="res-time">Choose time</label>
+        <select className="booking-form-input" {...register("time")}>
+          {availableTimes.map((time) => {
+            return (
+              <option key={time} value={time}>
+                {time}
+              </option>
+            );
+          })}
+        </select>
+        <p className="error-message">{errors.time?.message}</p>
+
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          className="booking-form-input"
+          {...register("numberOfGuests")}
+          type="number"
+          placeholder="0"
+          min="1"
+          max="10"
+        />
+        <p className="error-message">{errors.numberOfGuests?.message}</p>
+
+        <label htmlFor="occasion">Occasion</label>
+        <select className="booking-form-input" {...register("occasion")}>
+          <option>Select occasion</option>
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+        <p className="error-message">{errors.occasion?.message}</p>
+        <Button className="booking-form-input" type="submit">
+          Make Your reservation
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default BookingForm;
--- src/components/bookingForm/BookingForm.jsx
+++ src/components/bookingForm/BookingForm.jsx
@@ -1,73 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./bookingForm.css";
 import Button from "../button/Button";
-
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-// set up the validation schema for the form
-const schema = yup.object({
-  date: yup
-    .date()
-    .typeError("Select your reservation date")
-    .required("Select your reservation date"),
-  time: yup
-    .string()
-    .required("Select reservation hour.")
-    .test("is-not-default", "Select time", (value) => value !== "Select time"),
-  numberOfGuests: yup
-    .number()
-    .typeError("Choose a number of guests from 1 to 10.")
-    .positive("Choose a number of guests from 1 to 10.")
-    .integer("Choose a number of guests from 1 to 10.")
-    .required("Choose a number of guests from 1 to 10.")
-    .min(1, "Choose a number of guests from 1 to 10.")
-    .max(10, "Choose a number of guests from 1 to 10."),
-  occasion: yup
-    .string()
-    .required("Let us know the occasion.")
-    .test("is-not-default", "Select an occasion", (value) => value !== "Select occasion"),
-});
 
 const BookingForm = () => {
   const availableTimes = ["Select time", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 
-  // const [formContent, setFormContent] = useState({
-  //   date: "",
-  //   time: "",
-  //   numberOfGuests: "0",
-  //   occasion: "Select Occasion",
-  // });
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
+  const [formContent, setFormContent] = useState({
+    date: "",
+    time: "",
+    numberOfGuests: "0",
+    occasion: "Select Occasion",
+  });
 
   // console.log(watch("example")); // watch input value by passing the name of it
 
-  // const handleFormContent = (event) => {
-  //   setFormContent({ ...formContent, [event.target.name]: event.target.value });
-  //   console.log(event.target.value);
-  // };
+  const handleFormContent = (event) => {
+    setFormContent({ ...formContent, [event.target.name]: event.target.value });
+    console.log(event.target.value);
+  };
 
-  const sendFormContent = (data) => {
-    console.log(data);
+  const sendFormContent = (event) => {
+    event.preventDefault();
+    console.log(formContent);
   };
 
   return (
     <>
       <h1 className="subtitle">Reserve a Table</h1>
-      <form className="booking-form-container" onSubmit={handleSubmit(sendFormContent)}>
+      <form className="booking-form-container" onSubmit={sendFormContent}>
         <label htmlFor="res-date">Choose date</label>
-        <input className="booking-form-input" {...register("date")} type="date" />
-        <p className="error-message">{errors.date?.message}</p>
+        <input
+          className="booking-form-input"
+          type="date"
+          name="date"
+          onChange={handleFormContent}
+        />
 
         <label htmlFor="res-time">Choose time</label>
-        <select className="booking-form-input" {...register("time")}>
+        <select className="booking-form-input" name="time" onChange={handleFormContent}>
           {availableTimes.map((time) => {
             return (
               <option key={time} value={time}>
@@ -76,26 +46,25 @@ const BookingForm = () => {
             );
           })}
         </select>
-        <p className="error-message">{errors.time?.message}</p>
 
         <label htmlFor="guests">Number of guests</label>
         <input
           className="booking-form-input"
-          {...register("numberOfGuests")}
           type="number"
           placeholder="0"
           min="1"
           max="10"
+          name="numberOfGuests"
+          onChange={handleFormContent}
         />
-        <p className="error-message">{errors.numberOfGuests?.message}</p>
 
         <label htmlFor="occasion">Occasion</label>
-        <select className="booking-form-input" {...register("occasion")}>
+        <select className="booking-form-input" name="occasion" onChange={handleFormContent}>
           <option>Select occasion</option>
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
-        <p className="error-message">{errors.occasion?.message}</p>
+
         <Button className="booking-form-input" type="submit">
           Make Your reservation
         </Button>
