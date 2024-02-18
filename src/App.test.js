7ba17381e75d6177097f2790b4af36f74fab7bdf
rodriguezmauricio <contato.mauricioalves@gmail.com>
2024-02-18 18:33:06 +0000

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
 test("Render the app component", () => {
   render(<App />);
+});
+
+test("fetches available times and renders BookingPage", async () => {
+  render(<App />);
+  await waitFor(() => {
+    expect(screen.getByText("Reserve a table")).toBeInTheDocument();
+    // expect(screen.getAllByText(mockAvailableTimes.join("")).length).toBe(3);
+  });
 });
